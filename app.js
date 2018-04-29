e469e6af8e4e071ced0579ddba8275ecb060ad68
shalvah <diakon.ng@gmail.com>
2018-04-29 22:10:46 +0100

    Setup request logger

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const moment = require('moment');
+const RequestLog = require('./models/request_log');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+require('mongoose').connect('mongodb://localhost/poster');
 
-var app = express();
+app.use((req, res, next) => {
+    let requestTime = Date.now();
+    res.on('finish', () => {
+        if (req.path === '/analytics') {
+            return;
+        }
+
+        RequestLog.create({
+            url: req.path,
+            method: req.method,
+            responseTime: (Date.now() - requestTime) / 1000, // convert to seconds
+            day: moment(requestTime).format("dddd"),
+            hour: moment(requestTime).hour()
+        });
+    });
+    next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+require('hbs').registerHelper('toJson', data => JSON.stringify(data));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
